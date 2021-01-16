2b46078ff366950bb20829242522fd523f73c3e3
Mark <mk.gregy@gmail.com>
2021-01-16 23:05:15 +0800

    added db and cleaned up some default files

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::get('/task',function(){
+
+    $task = [
+        'task_name' => 'First',
+        'task_desc' => 'Task'
+    ];
+
+
+    return $task;
+
 });
+    
